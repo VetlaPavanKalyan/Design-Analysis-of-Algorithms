6f5302b1b8821e2693d1ba8e357ab68c52a0f535
VETLA PAVAN KALYAN <20131a4463@gmail.com>
2022-07-28 22:02:44 +0530

    Updated on 28th July 2022

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -1,30 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-	int i,j,temp,n;
-	cout<<"Enter size of the Array:";
-	cin>>n;
+	int i, j, temp, n;
+	cout << "Enter size of the Array:";
+	cin >> n;
 	int a[n];
-	cout<<"Enter elements of the Array:"<<endl;
-	for(i=0;i<n;i++)
+	cout << "Enter elements of the Array:" << endl;
+	for (i = 0; i < n; i++)
 	{
-		cin>>a[i];
+		cin >> a[i];
 	}
-	for(i=0;i<n-1;i++)
+	for (i = 0; i < n - 1; i++)
 	{
-		j=i+1;
-		while(j>0&&a[j-1]>a[j])
+		j = i + 1;
+		while (j > 0 && a[j - 1] > a[j])
 		{
-			swap(a[j-1],a[j]);
+			swap(a[j - 1], a[j]);
 			j--;
 		}
 	}
 	system("cls");
-	cout<<"Sorted Array is:"<<endl;
-	for(i=0;i<n;i++)
+	cout << "Sorted Array is:" << endl;
+	for (i = 0; i < n; i++)
 	{
-		cout<<a[i]<<"\t";
+		cout << a[i] << "\t";
 	}
 	return 0;
 }
